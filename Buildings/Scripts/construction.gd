c4bda138dda8f2430ce7853e9c876277aedf0b86
LogCheung <104600844+LogCheung@users.noreply.github.com>
2025-04-10 21:25:00 -0400

    build queue and nav removal when placed

--- Buildings/Scripts/construction.gd
+++ Buildings/Scripts/construction.gd
@@ -6,14 +6,27 @@ const STORAGE = preload("res://Buildings/Scenes/storage.tscn")
 @export var building : Buildings.BUILDINGS
 @export var timeToBuild : float = 5.0
 
+@onready var tiles : Node2D = get_node("/root/World/TileMap")
+@onready var grassTiles : TileMapLayer = tiles.grass
+@onready var treeTiles : TileMapLayer = tiles.tree
+@onready var north: Node2D = %North
 @onready var south: Node2D = %South
+@onready var west: Node2D = %West
+@onready var east: Node2D = %East
+@onready var points: Node2D = %Points
+@onready var area: Area2D = $Area2D
 
+var builder : Villager 
 
 func _ready() -> void:
 	Global.build_queue.append(self)
+	remove_nav_under()
 
 func _process(delta: float) -> void:
-	_handle_building_time(delta,building)
+	var villagers = area.get_overlapping_bodies()
+	if villagers.size() > 0:
+		builder = villagers[0]
+		_handle_building_time(delta,building)
 	
 func _handle_building_time(delta : float, building):
 	if timeToBuild <= 0.0:
@@ -34,3 +47,9 @@ func _build_chosen_building():
 			storage.global_position = global_position
 			get_parent().add_child(storage)
 	queue_free()
+
+func remove_nav_under() -> void:
+	for point : Node2D in points.get_children():
+		var pos = grassTiles.local_to_map(point.global_position)
+		grassTiles.set_cell(pos,0,Vector2i(0,1),1)
+	pass
--- NPC/Scripts/npc.gd
+++ NPC/Scripts/npc.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Villager
 
 enum LOOKING_FOR{
 	NONE, WOOD, BUILDING, BED
