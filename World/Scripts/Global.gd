de7d964091121457e9eae79c6bfd8dd37729c0b3
LogCheung <104600844+LogCheung@users.noreply.github.com>
2025-05-14 22:39:31 -0400

    Revert "merge fix"
    This reverts commit 6223f7780391e8c43138f35d5ab4b1c3b62965e4.

--- World/Scripts/Global.gd
+++ World/Scripts/Global.gd
@@ -26,11 +26,7 @@ enum VILLAGER_STATE{
 }
 
 var build_queue = []
-var drop_queue = []
 var tribute_payment : float
-var job_queues : Dictionary = {
-	JOB.LABORER : []
-}
 
 signal adult_count_increased
 
@@ -108,7 +104,7 @@ var job_name_dict : Dictionary = {
 	JOB.BLACKSMITH : "BLACKSMITH",
 }
 var job_limit_dict : Dictionary = {
-	JOB.FARMER : 0,
+	JOB.FARMER : 1000,
 	JOB.HUNTER : 0,
 	JOB.TANNER : 0,
 	JOB.MINER : 0,
